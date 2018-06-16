d43b48c3e7e91b556ecd53fa77c1ef57721cc971
NSTF <starlinu33@gmail.com>
2018-06-16 02:45:06 -0400

    first commit for my portfolio

---
+++ app.js
@@ -0,0 +1,16 @@
+const { Pool, Client } = require('pg');
+const express = require(`express`);
+const app = express();
+const port = 1890;
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+app.get(`/`, (req, res) => {
+  res.render('index', {
+
+  });
+});
+
+app.listen(port, (req, res) => {
+  console.log(`Listening to port ${port}...`);
+});
